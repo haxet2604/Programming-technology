a1efd36556982750d2d55b79418c7b0972f148c9
haxet2604 <machaon2604@proton.me>
2023-09-20 12:14:19 +0300

    feat: :fire: add user entity

---
+++ src/entity/user.cpp
@@ -0,0 +1,7 @@
+#include "user.h"
+
+User::User(string firstName, string surname, string lastName) {
+    this->first_name = firstName;
+    this->surname = surname;
+    this->last_name = lastName;
+};
